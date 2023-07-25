c2a1de424d0a7d2f1088bd63944c42393cf90e7a
김도연 <wkdal2965@naver.com>
2023-07-25 10:54:15 +0900

    20230725 2교시 머터리얼 디자인 탭 레이아웃, 뷰페이저 연동 및 플로팅 액션바

---
+++ test10_11_12/src/main/java/com/example/test10_11_12/MainActivity385.kt
@@ -0,0 +1,58 @@
+package com.example.test10_11_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.test10_11_12.databinding.ActivityMain385Binding
+import com.example.test10_11_12.fragment.OneFragment
+import com.example.test10_11_12.fragment.ThreeFragment
+import com.example.test10_11_12.fragment.TwoFragment
+import com.google.android.material.tabs.TabLayout
+
+//test12/src/main/java/com/example/test12/MainActivity385.kt
+//test12/src/main/res/layout/activity_main385.xml
+class MainActivity385 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMain385Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //탭 레이아웃 기본 설정
+        val tabLayout = binding.tabs
+
+        //탭 레이아웃과 프래그먼트를 연동하는 설정 코드
+        //시작시, 이 영역에 프레임 레이아웃 : R.id.tabContent -> OneFragment 프래그먼트 출력하겠다
+        supportFragmentManager.beginTransaction().add(R.id.tabContent, OneFragment()).commit()
+
+        //탭 클릭 시 이벤트 핸들러 설정 부분
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+            //탭 버튼 선택시 이벤트
+            //탭을 매개변수로 정의
+            //탭 클릭 시 해당 문자열을 기준으로 조건문 동작
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                //프래그먼트를 코드상으로 작업하겠다는 의미
+                val transaction = supportFragmentManager.beginTransaction()
+                when(tab?.text){
+                    //replace : 첫 매개변수 영역을 -> 두번재 영역으로 교체
+                    "Tab1"-> transaction.replace(R.id.tabContent, OneFragment())
+                    "Tab2"-> transaction.replace(R.id.tabContent, TwoFragment())
+                    "Tab3"-> transaction.replace(R.id.tabContent, ThreeFragment())
+                }
+                //이 부분이 수행되어야 동작함
+                transaction.commit()
+            }
+            //선택된 탭 버튼을 다시 선택하는 경우
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity385, "onTabReselected", Toast.LENGTH_SHORT).show()
+                Log.d("kkang", "onTabReselected........")
+            }
+            //선택된 탭버튼이 다른 탭 버튼을 눌러 선택 해제 되는 경우
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity385, "onTabUnselected", Toast.LENGTH_SHORT).show()
+                Log.d("kkang", "onTabUnselected........")
+            }
+        })
+    }
+}
---
+++ test10_11_12/src/main/java/com/example/test10_11_12/MainActivity389.kt
@@ -0,0 +1,48 @@
+package com.example.test10_11_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.test10_11_12.databinding.ActivityMain389Binding
+
+//test12/src/main/java/com/example/test12/MainActivity389.kt
+//test12/src/main/res/layout/activity_main389.xml
+
+//test12/src/main/res/layout/navigation_header.xml
+//추가 뷰 필요함  @layout/navigation_header
+
+//test12/src/main/res/menu/menu_navigation.xml
+// @menu/menu_navigation
+
+//플러팅 코드
+//ch12_material/src/main/res/layout/activity_main.xml
+class MainActivity389 : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMain389Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        binding.mainDrawerView.setNavigationItemSelectedListener {
+            Toast.makeText(this@MainActivity389, "navigation item click... ${it.title}", Toast.LENGTH_SHORT).show()
+            Log.d("kkang","navigation item click... ${it.title}")
+            true
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //이벤트가 toggle 버튼에서 제공된거라면..
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
